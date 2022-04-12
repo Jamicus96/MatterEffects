42f278b94109068824ddfea2c5c94b0e3afb861a
James Page <jepage999@gmail.com>
2022-04-12 22:53:46 +0100

    rewriting python wrapper for more consistency

--- run_loop_plot.py
+++ run_loop_plot.py
@@ -1,126 +1,162 @@
 ### RUN SURVIVAL PROB IN A LOOP AND PLOT ##
 
 import numpy as np
-import matplotlib.pyplot as plt
+import argparse
 import os
 
 
-
-####### TEST ######
-# Global variables
-GF = 1.663788e-14 #(eV^-2)
-hbar = 6.58211957e-16 #(eV.s)
-c = 299792458 #(m/s)
-
-s12 = np.sqrt(0.307)
-s13 = np.sqrt(2.18e-2)
-s23 = np.sqrt(0.545)
-c12 = np.sqrt(1.0 - s12 * s12)
-c13 = np.sqrt(1.0 - s13 * s13)
-c23 = np.sqrt(1.0 - s23 * s23)
-
-m21 = 7.53e-5 #(Delta m_21^2, in eV^2)
-m32 = 2.453e-3 #(Delta m_32^2, in eV^2)
-m31 = m32 + m21 #(Delta m_31^2, in eV^2)
-
-def Matt_Osc_m21(E, L, Ne):
-    '''Oscillations with matter effects, assuming \Delta m_{21}^2 dominance'''
-
-    # convert units to eV
-    E *= 1e6 #(MeV to eV)
-    L *= 1e3 / (c * hbar) #(km to eV^-1)
-    Ne *= c*c*c * hbar*hbar*hbar #(m^-3 to ev^3)
-
-    # Calculate Quantities
-    A_CC = -2 * np.sqrt(2) * E * GF * Ne
-
-    s_2thetaM_2 = np.sin(np.arctan(np.tan(2 * np.arcsin(s12)) / (1 - (c13*c13 * A_CC)/(np.cos(2 * np.arcsin(s12)) * m21))))
-
-    m_M21 = np.sqrt((m21 * np.cos(2 * np.arcsin(s12)) - c13*c13 * A_CC)**2 + (m21 * np.sin(2 * np.arcsin(s12)))**2)
-    m_M = 0.5 * (m21 + c13*c13 * A_CC + m_M21)
-
-    # Survival Probability
-    P = 1 - s_2thetaM_2**2 * np.sin((m_M * L)/(4 * E))**2
-    P = c13*c13*c13*c13 * P + s13*s13*s13*s13
-
-    return P
-
-def Matt_Osc_m31(E, L, Ne):
-    '''Oscillations with matter effects, assuming \Delta m_{21}^2 dominance'''
-
-    # convert units to eV
-    E *= 1e6 #(MeV to eV)
-    L *= 1e3 / (c * hbar) #(km to eV^-1)
-    Ne *= c*c*c * hbar*hbar*hbar #(m^-3 to ev^3)
-
-    # Calculate Quantities
-    A_CC = -2 * np.sqrt(2) * E * GF * Ne
-
-    s_2thetaM_2 = np.sin(np.arctan(np.tan(2 * np.arcsin(s13)) / (1 - A_CC/(np.cos(2 * np.arcsin(s13)) * m31))))
-
-    m_M = np.sqrt((m31 * np.cos(2 * np.arcsin(s13)) - A_CC)**2 + (m31 * np.sin(2 * np.arcsin(s13)))**2)
-
-    # Survival Probability
-    P = 1 - s_2thetaM_2**2 * np.sin((m_M * L)/(4 * E))**2
-
-    return P
-
-############################
+# Oscillation constants
+theta12 = np.arcsin(np.sqrt(0.307))
+theta13 = np.arcsin(np.sqrt(2.18e-2))
+theta23 = np.arcsin(np.sqrt(0.545))
+delta = 1.36 * np.pi
+m21 = 7.53e-5       # (Delta m_21^2, in eV^2)
+m31 = 0.0025283     # (Delta m_31^2, in eV^2)
+# m32 = m31 - m21     # (Delta m_32^2, in eV^2)
 
 
+# Optional input arguments
+def argparser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='Run matter effect neutrino oscillation in different modes.')
 
-# Create new file (delete first, if it exists)
-file_exists = os.path.exists('results.txt')
-if file_exists:
-    os.system('rm results.txt')
-open('results.txt', 'w')
+    parser.add_argument('--mass_hierarchy', '-m', type=str, dest='mass_hierarchy', choices=['NO', 'IO'],
+                        default='NO', help='Normal Ordering (NO) or Inverted Ordering (IO) [CURRENTLY NOT IMPLEMENTED].')
+    parser.add_argument('--antinu', '-a', type=bool, dest='antinu', choices=[True, False],
+                        default=False, help='True to simulate antineutrinos, False for neutrinos.')
+    parser.add_argument('--plot', '-p', type=bool, dest='plot', choices=[True, False],
+                        default=False, help="Plot results directly? Doesn't work on hpc")
+    args = parser.parse_args()
 
-# Run simulation for range of distances (prints results to file)
-N = 500
-P_21 = np.zeros(N)
-P_31 = np.zeros(N)
-L = np.linspace(0, 100, N)
-# rho_lithosphere = 2.7 #g/cm^3
-rho_lithosphere = 0
-Ne = rho_lithosphere / 4.821e-15 #electron density cm^-3
-Ne *= 1e6 #(cm^-3 to m^3)
-E = 1
-for i in range(N):
-    #print(i)
-    os.system("./Matter_Oscillations.exe {} {} {} 1".format(E, L[i], Ne))  #Input neutrino energy (MeV) and propagation length (km)
-    #P_21[i] = Matt_Osc_m21(E, L[i], Ne)
-    #P_31[i] = Matt_Osc_m31(E, L[i], Ne)
-
-# Read in results
-f = open('results.txt', 'r')
-data = np.loadtxt(f,skiprows=0)
-
-P = data[:,0]
-Pvac = data[:,1]
-Pglobes = data[:,2]
-Pvacglobes = data[:,3]
-
-#print(L)
-#print(Pvac)
-# E_eV = E * 1e6 #(MeV to eV)
-# Ne *= c*c*c * hbar*hbar*hbar #(m^-3 to ev^3)
-# A_CC = -2 * np.sqrt(2) * E_eV * GF * Ne
-
-# L_21 = int((2*np.pi * E_eV * hbar * c)/(m21 * 1000))
-# L_31 = int((2*np.pi * E_eV * hbar * c)/(m31 * 1000))
+    return args
 
 
-plt.plot(L, Pvac, label='Vacuum Case (my calc)')
-plt.plot(L, P, label='With Matter Effects (my calc)', linestyle='dashed')
-plt.plot(L, Pvacglobes, label='Vacuum Case (GLoBES)', linestyle='dashed')
-plt.plot(L, Pglobes, label='With Matter Effects (GLoBES)', linestyle='dashed')
-#plt.plot(L, P_21, label=r'$\Delta m_{21}^2(=7.53\times 10^{-5}$eV${}^2)$ dominance approximation')
-#plt.plot(L, P_31, label=r'$\Delta m_{31}^2(=2.53\times 10^{-3}$eV${}^2)$  dominance approximation')
-plt.xlabel("Baseline (km)")
-plt.ylabel(r"$\nu_\mu \rightarrow \nu_e$ transition probability")
-plt.legend(loc='best')
-Title = 'Comparing Survival Probability for {}MeV neutrino,\n\
-    with Matter Effects (constant density {}g/cm^3) to Vacuum Case'.format(E, rho_lithosphere)
-plt.title(Title)
-plt.show()
+def print_to_file(filename, info):
+    # If file exists, delete and recreate it
+    file_exists = os.path.exists(filename)
+    if file_exists:
+        os.system('rm ' + filename)
+    f = open(filename, 'w')
 
+    # Write to file line by line
+    if isinstance(info, list) or isinstance(info, np.ndarray):
+        for i in range(info):
+            f.write(info[i])
+    else:
+        f.write(info)
+
+
+def calculate_PMNS():
+    # Compute useful constants
+    s12 = np.sin(theta12)
+    s13 = np.sin(theta13)
+    s23 = np.sin(theta23)
+    c12 = np.cos(theta12)
+    c13 = np.cos(theta13)
+    c23 = np.cos(theta23)
+    c_delta = np.cos(delta)
+    s_delta = np.sin(delta)
+
+    # Real part of PMNS matrix
+    U_r = np.zeros((3, 3))
+    U_r[0, 0] = c12 * c13;                                  U_r[0, 1] = s12 * c13;                                  U_r[0, 2] = s13 * c_delta
+    U_r[1, 0] = - s12 * c23 - s13 * s23 * c12 * c_delta;    U_r[1, 1] = c12 * c23 - s12 * s13 * s23 * c_delta;      U_r[1, 2] = s23 * c13
+    U_r[2, 0] = s12 * s23 - s13 * c12 * c23 * c_delta;      U_r[2, 1] = - s23 * c12 - s12 * s13 * c23 * c_delta;    U_r[2, 2] = c13 * c23
+
+    # Imaginary part of PMNS matrix
+    U_i = np.zeros((3, 3))
+    U_i[0, 0] = 0.0;                                        U_i[0, 1] = 0.0;                                        U_i[0, 2] = - s13 * s_delta
+    U_i[1, 0] = - s13 * s23 * c12 * s_delta;                U_i[1, 1] = - s12 * s13 * s23 * s_delta;                U_i[1, 2] = 0.0
+    U_i[2, 0] = - s13 * c12 * c23 * s_delta;                U_i[2, 1] = - s12 * s13 * c23 * s_delta;                U_i[2, 2] = 0.0
+
+    return U_r, U_i
+
+
+def Compute_oscillations(L, E, rho, anti):
+    # Create new file (delete first, if it exists)
+    file_exists = os.path.exists('results.txt')
+    if file_exists:
+        os.system('rm results.txt')
+    open('results.txt', 'w')
+
+    ### Run simulation for each parameter ###
+    for i in range(len(L)):
+        os.system('./Matter_Oscillations.exe {} {} {} 1'.format(E, L[i], rho))
+
+
+def plot_results(L, E, rho):
+    ### IF PLOTTING ###
+    import matplotlib.pyplot as plt
+    # Read in results
+    f = open('results.txt', 'r')
+    data = np.loadtxt(f,skiprows=0)
+
+    P = data[:,0]
+    Pvac = data[:,1]
+    Pglobes = data[:,2]
+    Pvacglobes = data[:,3]
+
+    plt.plot(L, Pvac, label='Vacuum Case (my calc)')
+    plt.plot(L, P, label='With Matter Effects (my calc)', linestyle='dashed')
+    plt.plot(L, Pvacglobes, label='Vacuum Case (GLoBES)', linestyle='dashed')
+    plt.plot(L, Pglobes, label='With Matter Effects (GLoBES)', linestyle='dashed')
+    plt.xlabel("Baseline (km)")
+    plt.ylabel(r"$\nu_\mu \rightarrow \nu_e$ transition probability")
+    plt.legend(loc='best')
+    Title = 'Comparing Survival Probability for {}MeV neutrino,\n\
+        with Matter Effects (constant density {}g/cm^3) to Vacuum Case'.format(E, rho)
+    plt.title(Title)
+    plt.show()
+
+
+def main():
+    # Get arguments
+    args = argparser()
+
+    # Print oscillation constants to file, to be used by C++ code, ensuring consistency
+    osc_consts = [theta12, theta13, theta23, delta, m21, m31]
+    print_to_file('oscillation_constants.txt', osc_consts)
+
+    # Compute PMNS matrix elements and print them to file (for vacuum oscillation calculation)
+    U_r, U_i = calculate_PMNS()
+    PMNS_elements = []
+    for i in range(3):
+        for j in range(3):
+            PMNS_elements.append(U_r[i][j])
+    for i in range(3):
+        for j in range(3):
+            PMNS_elements.append(U_i[i][j])
+    print_to_file('PMNS_m2_data.txt', PMNS_elements)
+
+    # Compute constants needed for algorithm, outside of loop
+    os.system('./Mat_Os_Consts.exe')
+
+    ### Simulation parameters ###
+    N = 500
+    L = np.linspace(0.0, 100.0, N)
+    E = 1.0
+    rho_lithosphere = 2.7  #g/cm^3
+    rho = 0.0  #g/cm^3
+
+    # Run simulation
+    Compute_oscillations(L, E, rho, args.antinu)
+
+    # Plot results, if desired
+    if args.plot:
+        plot_results(L, E, rho)
+
+
+
+
+#  Convert matter density to electron density using 
+#  converstion factors from GLOBES-3.0.11/src/glb_probability.h:
+#  GLB_V_FACTOR        7.5e-14   /* Conversion factor for matter potentials */
+#  GLB_Ne_MANTLE       0.5        /* Effective electron numbers for calculation */
+#  Also note:
+#  V_THRESHOLD 0.001*GLB_V_FACTOR*GLB_Ne_MANTLE  /* The minimum matter potential below which vacuum algorithms are used */
+#  Anyway, used to convert from matter density rho (g/cm^3) to matter potential V (glb_probability.c),
+#  Where: V = sqrt(2)*G_F*N_e, and is given in eV
+
+# Ne = rho / 4.821e-15 #electron density cm^-3
+# Ne *= 1e6 #(cm^-3 to m^3)
