4a8723741f79644680cc3f2d69ce06aa48265f8e
James Page <jepage999@gmail.com>
2022-04-12 23:00:43 +0100

    tweaked constant calculation code to match python script

--- Mat_Os_Consts.cpp
+++ Mat_Os_Consts.cpp
@@ -13,7 +13,6 @@
 #include <cmath>
 #include <fstream>
 #include <string>
-//#include "cplx.hpp"
 
 
 void Survival_Prob_Constants(std::string filename, double data[20]);
@@ -26,9 +25,9 @@ int main() {
     double Nwater = 3.3e29;
 
     // Read in data from text file
-    double* datapoint = read_data("PMNS_m2_data.txt", 20);
-    double data[20];
-    for(int i=0; i<20; ++i){
+    double* datapoint = read_data("oscillation_constants.txt", 20);
+    double data[6];
+    for(int i=0; i<6; ++i){
         data[i] = *(datapoint + i);
     }
 
@@ -37,7 +36,6 @@ int main() {
 
     return 0;
 }
-
 
 
 /**
@@ -62,55 +60,33 @@ double* read_data(std::string filename, int num) {
     return array;
 }
 
-
 /* ------------------------------------------------------- */
-
 
 void Survival_Prob_Constants(std::string filename, double data[20]) {
     // Unpack data
-    double U_Re[3][3];
-    double U_Im[3][3];
-    for (int i=0; i<3; ++i) {
-        for (int j=0; j<3; ++j) {
-            U_Re[i][j] = data[3*i + j];
-            U_Im[i][j] = data[9 + 3*i + j];
+    double theta12 = data[0];
+    double theta13 = data[1];
+    double theta23 = data[2];
+    double delta = data[3];
+    double m21 = data[4];
+    double m31 = data[5];
 
-            std::cout << "U_" << i << j << " = " << U_Re[i][j] << "+ i" << U_Im[i][j] << std::endl;
-        }
-    }
-    for (int k=0; k<3; ++k) {
-        std::cout << "U_e" << k+1 << "U_mu" << k+1 << "^* = " << U_Re[0][k] * U_Re[1][k] + U_Im[0][k] * U_Im[1][k]
-            << " + i" << U_Im[0][k] * U_Re[1][k] - U_Re[0][k] * U_Im[1][k] << std::endl;
-    }
-    double m21 = data[18];
-    double m31 = data[19];
-
-    std::cout << "m21 = " << m21 << std::endl;
-    std::cout << "m31 = " << m31 << std::endl;
+    // Useful constants
+    double s12 = std::sin(theta12);
+    double s13 = std::sin(theta13);
+    double s23 = std::sin(theta23);
+    double c12 = std::cos(theta12);
+    double c13 = std::cos(theta13);
+    double c23 = std::cos(theta23);
 
     // Compute vacuum constants
     double a0 = - (2.0/27.0) * (m21*m21*m21 + m31*m31*m31) + (1.0/9.0) * (m21*m21 * m31 + m21 * m31*m31);
     double a1 = (1.0/3.0) * (m21 * m31 - m21*m21 - m31*m31);
 
-    // Input parameters
-    double s12 = sqrt(0.297);
-    double s13 = sqrt(0.0215);
-    double s23;
-    if(true){
-        s23 = sqrt(0.545);
-    } else {
-        s23 = sqrt(0.547);
-    }
-    double c12 = sqrt(1.0 - s12 * s12);
-    double c13 = sqrt(1.0 - s13 * s13);
-    double c23 = sqrt(1.0 - s23 * s23);
-
     double H_ee = m21 * (s12*s12 * c13*c13 - (1.0/3.0)) + m31 * (s13*s13 - (1.0/3.0));
 
     double H_neq2 = c13*c13 * (m21*m21 * s12*s12 * (c12*c12 + s12*s12 * s13*s13) + m31*m31 * s13*s13
                     - 2.0 * m21 * m31 * s12*s12 * s13*s13);
-
-    std::cout << H_neq2 << std::endl;
 
     double Y_ee = (2.0/3.0) * a1 + H_ee*H_ee + H_neq2;
 
@@ -138,6 +114,7 @@ void Survival_Prob_Constants(std::string filename, double data[20]) {
     datafile << R_Y_em << "\n";
     datafile << I_Y_em << "\n";
 }
+
 
 double Real_Yem(double m21, double m31, double s12, double s13, double s23, double delta) {
 
